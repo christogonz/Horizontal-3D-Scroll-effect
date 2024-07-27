3d2f3f92ee9954533a4361f845688c99c7b5f242
Chris Gonzalez <christogonz92@gmail.com>
2024-07-27 10:40:07 +0200

    Initial Commit

---
+++ Horizontal3DScroll/Horizontal3DScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Horizontal3DScrollApp.swift
+//  Horizontal3DScroll
+//
+//  Created by Christopher Gonzalez on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct Horizontal3DScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
