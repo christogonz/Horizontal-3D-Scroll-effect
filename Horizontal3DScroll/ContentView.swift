0109e774a3c3f077506b298b0d39931e6756787d
Chris Gonzalez <christogonz92@gmail.com>
2024-07-27 11:26:38 +0200

    [Feature] Horozontal 3D Scroll Effect

--- Horizontal3DScroll/ContentView.swift
+++ Horizontal3DScroll/ContentView.swift
@@ -9,16 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach(0 ..< 5) { item in
+                    GeometryReader { geometry in
+                        ZStack {
+                            
+                            RoundedRectangle(cornerRadius: 30)
+                                .fill(LinearGradient(
+                                    gradient: Gradient(colors: [Color.blue, Color.purple]),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing))
+                                
+                            
+                            Image(systemName: "person")
+                                .font(.system(size: 100))
+                        }
+                        .rotation3DEffect(
+                            Angle(
+                                degrees: geometry.frame(
+                                in: .global).minX - 16) / -20,
+                            axis: (x: 0.0, y: 1.0, z: 0.0),
+                            anchor: .center,
+                            anchorZ: 0,
+                            perspective: 1
+                        )
+                        
+                        
+                    }
+                    .frame(width: 300, height: 300)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
